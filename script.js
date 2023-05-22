86faab4e57402e18610276a2fff892522c8303e9
henry <hjbishop@live.co.uk>
2023-05-22 14:45:56 +0900

    re-code basic logic, tidy up syntax, add reset button, finish basic input and display

--- script.js
+++ script.js
@@ -1,16 +1,70 @@
-let gameBoard = {
-  board: ["", "", "", "", "", "", "", "", ""]
+let gameBoardLogic = (() => {
+  let mainBoard = document.querySelector(".grid-container");
+  let boxes = [...mainBoard.querySelectorAll('.block')];
+  
+  
+    
+  let gameBoard =  (() => {
+    let board = ["", "", "", "", "", "", "", "", ""];
 
-};
+    return {board};
+  })();
+
   console.log(gameBoard.board.length)
+  let counter = 0;
+  boxes.forEach((element) => {
+    element.onclick = () => {
+ 
+ 
+      let id = element.getAttribute("id")
+      if (counter % 2 == 0) {
+        gameBoard.board[parseInt(id)] = player1.sign;
+        element.innerHTML = gameBoard.board[parseInt(id)];
+        console.log(gameBoard.board);
+        counter++;
+        console.log(counter);
+      }
+      else {
+        gameBoard.board[parseInt(id)] = player2.sign;
+        element.innerHTML = gameBoard.board[parseInt(id)];
+        console.log(gameBoard.board);
+        counter++;
+        console.log(counter);
+      }
+    
+    }
+  
+  }); 
 
-   let starterGrid = (() => {
-      for (let i = 0; i < gameBoard.board.length; i++) {
-        let container = document.querySelector(".grid-container");
-        let grid = document.createElement('div');
-        grid.classList = ('block');
-        grid.setAttribute("id", i)
-        container.appendChild(grid);
-      }}
-   )();
+  let reset = (() => {
+    btn = document.getElementById("reset");
+    btn.onclick = () => {
+      counter = 0;
+      console.log(counter);
+      gameBoard.board = ["", "", "", "", "", "", "", "", ""];
+      console.log(gameBoard.board);
+      boxes.forEach((element) => { 
+        element.innerHTML = "";
+      })
+    
+    }
+  })();
+
+
+  let Player = (name, sign) => {
+
+    const getsign = () => sign;
+    const getName  = () => name;
+    //console.log(`${name} and the sign is ${sign}`);
+    return {name, sign};
+  }
+
+  let player1 = Player('player one', "X");
+  let player2 = Player('player two', "O");
+
+})();
+
+
+
+
 
